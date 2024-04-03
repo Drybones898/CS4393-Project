b9102d3d88e3614036dad5b7d4483ec6aa2d7173
Skyler <100170306+Drybones898@users.noreply.github.com>
2024-04-03 17:47:40 -0500

    Simple Options Menu
    Added simple options menu and added some main menu functionality. Unsure of how to get screen reader to work with dropdowns.

---
+++ CS4393-Project/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public ScreenReader ScreenReader;
+    [Header("Options Stuff")]
+    public TMP_Dropdown resolutionDropdown;
+    public GameObject optionsMenu;
+    public GameObject mainMenu;
+    public Button startButton;
+    public Button optionsButton;
+    public Button confirmButton;
+    public Button quitButton;
+    public Toggle fullscreenToggle;
+    Resolution[] resolutions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Add listener for when the value of the Dropdown changes, to take action
+        resolutionDropdown.onValueChanged.AddListener(delegate {
+            onResolutionChanged(resolutionDropdown);
+        });
+        startButton.onClick.AddListener(startGame);
+        optionsButton.onClick.AddListener(toOptions);
+        confirmButton.onClick.AddListener(toMainMenu);
+        quitButton.onClick.AddListener(Quit);
+        
+        fullscreenToggle.onValueChanged.AddListener(delegate {
+            SetFullScreen();
+        });
+
+        fullscreenToggle.isOn = Screen.fullScreen;
+        resolutions = Screen.resolutions;
+        resolutionDropdown.options = new List<TMP_Dropdown.OptionData>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string resolutionString = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRateRatio + "Hz";
+            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolutionString));
+
+            //set to be our default
+            if (PlayerPrefs.GetInt("set default resolution") == 0)
+            {
+                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                {
+                    resolutionDropdown.value = i;
+                    PlayerPrefs.SetInt("set default resolution", 1);
+                    string resolutionString2 = resolutions[resolutionDropdown.value].width + "x" + resolutions[resolutionDropdown.value].height + " " + resolutions[resolutionDropdown.value].refreshRateRatio + "Hz";
+                    ScreenReader.StaticReadText(resolutionString2);
+                    print(resolutionString2);
+                    SetResolution();
+                }
+            }
+        }
+        resolutionDropdown.value = PlayerPrefs.GetInt("resolution");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onResolutionChanged(TMP_Dropdown change)
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+        PlayerPrefs.SetInt("resolution", resolutionDropdown.value);
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene("Placeholder");
+    }
+
+    void toOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    void toMainMenu()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SetResolution()
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+        PlayerPrefs.SetInt("resolution", resolutionDropdown.value);
+    }
+    
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
+    }
+}
